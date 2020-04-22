b5e235c49e1c60fa7cee4895bad2ea1ac7741c79
nobobo1234 <noahvanboven@gmail.com>
2020-04-22 09:18:45 +0200

    Working on natours

--- natours/models/userModel.js
+++ natours/models/userModel.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
@@ -39,7 +40,9 @@ const userSchema = mongoose.Schema({
             message: 'Passwords are not the same!'
         }
     },
-    passwordChangedAt: Date
+    passwordChangedAt: Date,
+    passwordResetToken: String,
+    passwordResetExpires: Date
 });
 
 userSchema.pre('save', async function(next) {
@@ -48,6 +51,13 @@ userSchema.pre('save', async function(next) {
     this.password = await bcrypt.hash(this.password, 12);
 
     this.passwordConfirm = undefined;
+    next();
+});
+
+userSchema.pre('save', function(next) {
+    if (!this.isModified('password') || this.isNew) return next();
+
+    this.passwordChangedAt = Date.now() - 1000; // Doing -1000 because the new token can actually be created before database save
     next();
 });
 
@@ -70,6 +80,19 @@ userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
 
     // FALSE MEANS NOT CHANGED
     return false;
+};
+
+userSchema.methods.createPasswordResetToken = function() {
+    const resetToken = crypto.randomBytes(32).toString('hex');
+
+    this.passwordResetToken = crypto
+        .createHash('sha256')
+        .update(resetToken)
+        .digest('hex');
+
+    this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+    return resetToken;
 };
 
 const User = mongoose.model('User', userSchema);
