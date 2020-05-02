1e89bd2bdf5eb180ed5db874881e15a73481aeaa
nobobo1234 <noahvanboven@gmail.com>
2020-05-02 20:11:39 +0200

    Improved stripe implementation

--- natours/app.js
+++ natours/app.js
@@ -17,6 +17,7 @@ const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const viewRouter = require('./routes/viewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
+const bookingController = require('./controllers/bookingController');
 
 const app = express();
 
@@ -47,6 +48,12 @@ const limiter = rateLimit({
 
 // Rate limiter
 app.use('/api', limiter);
+
+app.post(
+    '/webhook-checkout',
+    express.raw({ type: 'application/json' }),
+    bookingController.webhookCheckout
+);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
