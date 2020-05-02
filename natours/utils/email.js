36c87c02dc325b12be3ed2f44281e1e7c3bc493e
nobobo1234 <noahvanboven@gmail.com>
2020-05-02 20:36:03 +0200

    Improving email

--- natours/utils/email.js
+++ natours/utils/email.js
@@ -11,13 +11,10 @@ module.exports = class Email {
     }
 
     newTransport() {
-        if (process.env.NODE_ENV === 'production') {
-            return 1;
-        }
-
         return nodemailer.createTransport({
             host: process.env.EMAIL_HOST,
             port: process.env.EMAIL_PORT,
+            secure: true,
             auth: {
                 user: process.env.EMAIL_USERNAME,
                 pass: process.env.EMAIL_PASSWORD
