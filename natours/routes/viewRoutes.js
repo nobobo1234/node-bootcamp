e07746b326ceba694235758fe1c61c60a2bc58e7
nobobo1234 <noahvanboven@gmail.com>
2020-05-02 20:15:02 +0200

    cleaning route

--- natours/routes/viewRoutes.js
+++ natours/routes/viewRoutes.js
@@ -1,15 +1,10 @@
 const express = require('express');
 const authController = require('../controllers/authController');
 const viewsController = require('../controllers/viewsController');
-const bookingController = require('../controllers/bookingController');
 
 const router = express.Router();
 
-router.get(
-    '/',
-    authController.isLoggedIn,
-    viewsController.getOverview
-);
+router.get('/', authController.isLoggedIn, viewsController.getOverview);
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
 router.get('/signup', authController.isLoggedIn, viewsController.getSignupForm);
