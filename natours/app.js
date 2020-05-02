bd806af856273bb3be634649466c63327aa0e3e8
nobobo1234 <noahvanboven@gmail.com>
2020-05-02 19:54:31 +0200

    Adding cors

--- natours/app.js
+++ natours/app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -23,6 +24,10 @@ app.enable('trust proxy');
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
+
+// SETTING CORS
+app.use(cors());
+app.options('*', cors());
 
 // Middlewares
 // Serving status files
